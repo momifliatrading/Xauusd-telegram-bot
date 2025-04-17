d33c67a354bb8f54c51a7c43ca6a9c3619cc104c
momifliatrading <momyflia98@gmail.com>
2025-04-17 09:25:50 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -6,3 +6,11 @@ def get_price():
     if data.empty:
         return None
     return round(data['Close'].iloc[-1], 2)
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+# Messaggio di test
+requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text=✅+Bot+collegato+correttamente!")
