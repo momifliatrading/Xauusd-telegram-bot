787c546df85389a5fa223ef036d5a7a963ea1ba1
momifliatrading <momyflia98@gmail.com>
2025-04-17 08:41:09 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,13 @@ import yfinance as yf
 import time
 
 def get_xauusd_data():
-    try:
-        ticker = yf.Ticker("GC=F")  # Future sull'oro, più stabile
-        data = ticker.history(period="1d", interval="1m")
-
-        if not data.empty:
-            last_price = data["Close"].iloc[-1]
-            print(f"Prezzo attuale XAU/USD: {last_price}")
-        else:
-            print("Nessun dato ricevuto.")
-
-    except Exception as e:
-        print(f"Errore durante il recupero dei dati: {e}")
+    ticker = yf.Ticker("XAUUSD=X")
+    data = ticker.history(period="1d", interval="1m")
+    if not data.empty:
+        last_price = data["Close"].iloc[-1]
+        print(f"Prezzo attuale XAU/USD: {last_price}")
+    else:
+        print("Nessun dato ricevuto.")
 
 while True:
     get_xauusd_data()
